5cfcbc8e1380991588a6b6383444d23ab1b6e202
Michael Richard <mrichard@sat.qc.ca>
2015-08-25 17:12:14 -0400

    upgrade to docker 1.8

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 _install () {
-  apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys F76221572C52609D
 
   cat > /etc/apt/sources.list.d/docker.list <<- EOF
-	deb https://get.docker.com/ubuntu docker main
 	deb https://apt.dockerproject.org/repo debian-jessie main 
 EOF
 
   apt-get update -q
-  apt-get install -y lxc-docker python-pip
+  apt-get install -y docker-engine python-pip
 
   pip install -U docker-compose
 
